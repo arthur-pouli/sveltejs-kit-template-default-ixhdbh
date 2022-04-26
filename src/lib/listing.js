f508fdbacac42ab8eb188f786d130b2cb6126a4b
arthur-pouli <poulignyarthur1313@gmail.com>
2022-04-26 16:16:12 +0200

    Initial commit

---
+++ src/lib/listing.js
@@ -0,0 +1,24 @@
+const bornes = [
+	{
+		name : "Borne EIGSI",
+		adresse : "26 Rue François de Vaux de Foletier, 17041 La Rochelle",
+		nbrPlace : 10,
+		nbrPlaceDispo : 5,
+		id :1,
+	},
+	{
+		name : "Borne aquarium",
+		adresse : "Quai Louis Prunier, 17000 La Rochelle",
+		nbrPlace : 15,
+		nbrPlaceDispo : 3,
+		id :2,
+	},
+	{
+		name : "Borne CGR",
+		adresse : "2 Place Jean Louis Rieupeyrout, Av. Henri Becquerel, 17000 La Rochelle",
+		nbrPlace : 10,
+		nbrPlaceDispo : 2,
+		id :3,
+	},
+];
+export default bornes;
---
+++ src/routes/[id].svelte
@@ -0,0 +1,11 @@
+<script>
+  import { page } from '$app/stores';
+  import bornes from '$lib/listing.js'
+
+  let borne = bornes.find(borne => borne.id === parseInt($page.params.id))
+   
+</script>
+
+{#if borne}
+  <div>{borne.name}</div>
+{/if}
